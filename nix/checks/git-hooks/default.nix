6d2a066881afaa8edb7868f8934d07fc2eb92e8a
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-12 19:43:18 +0100

    add plenty of dev-time checks

--- nix/checks/git-hooks/default.nix
+++ nix/checks/git-hooks/default.nix
@@ -1,13 +1,21 @@
 {
   inputs,
-  pkgs,
   system,
   ...
 }:
-inputs.git-hooks-nix.lib.${pkgs.system}.run {
+inputs.git-hooks-nix.lib.${system}.run {
   src = inputs.self.outPath;
-  hooks.treefmt = {
-    enable = true;
-    packageOverrides.treefmt = inputs.self.formatter.${system};
+  hooks = {
+    actionlint.enable = true;
+    detect-private-keys.enable = true;
+    end-of-file-fixer.enable = true;
+    flake-checker.enable = true;
+    # no commit to main branch, force use of PRs.
+    no-commit-to-branch.enable = true;
+    statix.enable = true;
+    treefmt = {
+      enable = true;
+      packageOverrides.treefmt = inputs.self.formatter.${system};
+    };
   };
 }
