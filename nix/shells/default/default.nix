fca584b028a163001f1d9ba10a6e6fe33985ed9b
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-12 19:43:18 +0100

    add plenty of dev-time checks

--- nix/shells/default/default.nix
+++ nix/shells/default/default.nix
@@ -8,17 +8,18 @@
 # The 'default' devShell can be invoked manually with `nix develop` from the flake directory, and
 # direnv integration means that cd'ing into this dev directory (after allow-listing)
 # automatically loads the relevant development shell + helpful tools.
-mkShell {
+let
   # Note: this refers to our checks/git-hooks module.
-  inherit (inputs.self.checks.${system}.git-hooks) shellHook;
+  check = inputs.self.checks.${system}.git-hooks;
+in
+mkShell {
+  inherit (check) shellHook;
 
   packages = [
     inputs.agenix-rekey.packages.${system}.default
     # Note: pgs.colmena below would be too old
     inputs.colmena.defaultPackage.${system}
-  ];
-
-  buildInputs = [
     pkgs.age-plugin-fido2-hmac
   ];
+  buildInputs = check.enabledPackages;
 }
