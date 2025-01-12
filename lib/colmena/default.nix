f47bc38b843282e6b405488e4ff98cbdce4fa7b6
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-12 15:58:38 +0100

    Find ways to actually pass colmena deployment configs

---
+++ lib/colmena/default.nix
@@ -0,0 +1,21 @@
+{ inputs, ... }:
+{
+  mkColmenaHive =
+    nixpkgs: deployments:
+    let
+      confs = inputs.self.nixosConfigurations;
+      colmenaConf =
+        {
+          meta = {
+            inherit nixpkgs;
+            nodeNixpkgs = builtins.mapAttrs (_name: value: value.pkgs) confs;
+            nodeSpecialArgs = builtins.mapAttrs (_name: value: value._module.specialArgs) confs;
+          };
+        }
+        // builtins.mapAttrs (name: value: {
+          imports = value._module.args.modules;
+          deployment = deployments.${name} or { };
+        }) confs;
+    in
+    inputs.colmena.lib.makeHive colmenaConf;
+}
