95ddd263347578b013f4bdedf5bb773615b1817d
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-13 10:59:41 +0100

    vm: bootloader seems to work now

--- nix/systems/x86_64-linux/qemu/default.nix
+++ nix/systems/x86_64-linux/qemu/default.nix
@@ -1,19 +1,5 @@
 { inputs, ... }:
 {
-  # This was useful to mount a shared folder with the host, but I pass around nix configs
-  # differently now. Keeping for reference.
-  # Note that virtualisation has support
-  # fileSystems."/media/host" = {
-  #   device = "shared0";
-  #   fsType = "9p";
-  #   options = [
-  #     "trans=virtio"
-  #     "version=9p2000.L"
-  #     "posixacl"
-  #     "cache=mmap"
-  #   ];
-  # };
-
   # Note: I do this instead of using the snowfall-lib integration "magic" because this then creates
   # the host as a nixosConfigurations which is then part of builds / CI / etc. (vmConfigurations is
   # not recognized).
@@ -27,10 +13,10 @@
         guest.port = 22;
       }
     ];
-    writableStore = true;
-    # NOTE: virtualisation.mountHostNixStore uses `!cfg.useNixStoreImage && !cfg.useBootLoader` as a
-    # default and I imagine this may be why this breaks
-    #useBootLoader = true;
+    mountHostNixStore = true;
+    useBootLoader = true;
     diskSize = 4 * 1024;
+    useEFIBoot = true;
+    # NOTE: sharedDirectories = is useful if I need to share with the host.
   };
 }
