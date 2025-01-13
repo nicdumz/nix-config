f709afe9c4aeb1083d7fb2f5273ac7302696bdb5
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-13 10:49:57 +0100

    use `nixosConfigurations` instead of `vm.*Configurations`

--- nix/systems/x86_64-linux/nixosvm/qemu-guest.nix
+++ nix/systems/x86_64-linux/nixosvm/qemu-guest.nix
@@ -13,18 +13,6 @@
     "ata_piix"
   ];
 
-  # This was useful to mount a shared folder with the host, but I pass around nix configs
-  # differently now. Keeping for reference.
-  # fileSystems."/media/host" = {
-  #   device = "shared0";
-  #   fsType = "9p";
-  #   options = [
-  #     "trans=virtio"
-  #     "version=9p2000.L"
-  #     "posixacl"
-  #     "cache=mmap"
-  #   ];
-  # };
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
 }
