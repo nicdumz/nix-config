408b8839ec200a114a5a4348edbe7e8a5b081e6e
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-13 20:15:06 +0100

    vm with bootloader -- unfortunately not an option, sad.

--- vm.nix
+++
@@ -1,8 +0,0 @@
-# This loads a vm in the current shell:
-#   nix build vm.nix && ./result/bin/run-qemu-vm
-# Log into it via ssh:
-#   ssh -p 2222 root@localhost
-let
-  flake = builtins.getFlake (toString ./.);
-in
-flake.nixosConfigurations.qemu
