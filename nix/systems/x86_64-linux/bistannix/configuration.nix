e57789e44c4218a880b3726c17cb87817db0a9b4
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-13 07:39:44 +0100

    reorganizing hardware config some

--- nix/systems/x86_64-linux/bistannix/configuration.nix
+++ nix/systems/x86_64-linux/bistannix/configuration.nix
@@ -1,34 +1,7 @@
-{
-  config,
-  lib,
-  modulesPath,
-  ...
-}:
-{
-  boot.initrd = {
-    kernelModules = [
-      "nvme"
-      "usbhid" # fido2 over usb
-      # yubico
-      "nls_cp437"
-      "nls_iso8859-1"
-    ];
-    availableKernelModules = [
-      "xhci_pci"
-      "ahci"
-      "nvme"
-      "usbhid"
-      "usb_storage"
-      "sd_mod"
-    ];
-  };
+_: {
+  # Will run VMs.
   boot.kernelModules = [ "kvm-intel" ];
 
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   # Module for network:
   #         # 10G network card
   #         "atlantic"
