469d4af4ef650dfaf7b323eac08eb6e68c958c38
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-14 17:49:38 +0100

    Latest changes, in particular add relevant vscode extensions etc.

--- nix/systems/x86_64-linux/bistannix/default.nix
+++ nix/systems/x86_64-linux/bistannix/default.nix
@@ -17,4 +17,21 @@
     graphical = true;
     persistence.enable = true;
   };
+
+  # TODO: package in a module
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
+    loadModels = [ "qwen2.5-coder:1.5b" ];
+  };
+
+  # Cuda (for ollama) is unfree.
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "cuda_cccl"
+      "cuda_cudart"
+      "cuda_nvcc"
+      "libcublas"
+    ];
 }
