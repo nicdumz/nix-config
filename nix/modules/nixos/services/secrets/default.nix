667e67b6bac2d985df7364de8991cf513ac3d79b
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-16 20:27:44 +0100

    And the last magic trick to make user passwords work is...  (success!)

--- nix/modules/nixos/services/secrets/default.nix
+++ nix/modules/nixos/services/secrets/default.nix
@@ -4,4 +4,6 @@
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
   sops.defaultSopsFile = inputs.self.outPath + "/secrets/global.yaml";
+  # Due to impermanence, need to make sure the SSH keys appear early enough.
+  fileSystems."/etc/ssh".neededForBoot = true;
 }
