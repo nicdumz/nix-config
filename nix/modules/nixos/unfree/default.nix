366b046cc1b89e4248d034be6df41a865a0929d7
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-18 22:18:46 +0100

    Allow unfree, it just is what it is.

--- nix/modules/nixos/unfree/default.nix
+++
@@ -1,8 +0,0 @@
-_: {
-  # TODO: Modularize so in theory this + nvidia enabling works.
-  #  nixpkgs.config.allowUnfreePredicate =
-  #    pkg:
-  #    builtins.elem (lib.getName pkg) (
-  #      lib.optionals config.snowfallorg.users.giulia.create [ "google-chrome" ]
-  #    );
-}
