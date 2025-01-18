ae7d8ba9c2301221463803fc58d5ed515b3ca03f
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-18 22:00:47 +0100

    Use VSCode snippets instead of templates, much simpler.

--- nix/packages/initHomeModule/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-pkgs.writeTextFile {
-  name = "initHomeModule";
-  text = ./initHomeModule.fish;
-  destination = "/share/fish/vendor_functions.d/initHomeModule.fish";
-  executable = true;
-}
