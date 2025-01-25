fb4e42420b128fde4572d3f3babb0349a2a27ca0
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-25 16:33:40 +0100

    Small fixes for blocky, e.g. there was a case typo.

--- nix/modules/nixos/services/blocky/default.nix
+++ nix/modules/nixos/services/blocky/default.nix
@@ -16,93 +16,99 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
-    services.blocky.enable = true;
     networking.resolvconf.useLocalResolver = true;
+
+    # TODO: why is this even enabled in the first place...
+    services.resolved.enable = false;
+
     # No need to start before network.
     systemd.services.blocky.after = [ "network-online.target" ];
     systemd.services.blocky.wants = [ "network-online.target" ];
-    services.blocky.settings = {
-      bootstrapDns = [
-        {
-          upstream = "tcp-tls:dns.quad9.net";
-          ips = [
-            "9.9.9.9"
-            "149.112.112.112"
-            "2620:fe::fe"
-            "2620:fe::9"
-          ];
-        }
-      ];
-      upstreams.groups.default = [ "tcp-tls:dns.quad9.net" ];
-      customDNS = {
-        customTTL = "1m";
-        mapping = {
-          lethargy = "192.168.1.1";
-          "jonsnow.local" = "192.168.1.1";
-          bistannix = "192.168.1.42";
-          "home.nicdumz.fr" = "192.168.1.1";
-        };
-      };
-      caching.prefetching = true;
-      queryLog = {
-        type = "csv";
-        logRetentionDays = 7;
-        target = "/var/log/blocky";
-      };
-      ports.http = 4000;
-      prometheus.enable = true;
-      blocking = {
-        # NOTE: blocking was disabled for a while.
-        clientGroupsBlock.default = [
-          "ads"
-          "suspicious"
-          "tracking"
+    services.blocky = {
+      enable = true;
+      settings = {
+        bootstrapDns = [
+          {
+            upstream = "tcp-tls:dns.quad9.net";
+            ips = [
+              "9.9.9.9"
+              "149.112.112.112"
+              "2620:fe::fe"
+              "2620:fe::9"
+            ];
+          }
         ];
-        # https://firebog.net/ was useful here
-        allowLists = {
-          ads = [
-            # inline definition of hosts to allow
-            ''
-              bit.ly
-              bnc.lt
-              cdn.optimizely.com
-              ow.ly
-              s.shopify.com
-              t.ly
-              tinyurl.com
-              www.bit.ly
-              # https://github.com/StevenBlack/hosts/issues/1206 sigh both insta and
-              # tracking
-              graph.instagram.com
-              # this is what M$ uses to phone home / check connectivity
-              dns.msftncsi.com''
-          ];
+        upstreams.groups.default = [ "tcp-tls:dns.quad9.net" ];
+        customDNS = {
+          customTTL = "1m";
+          mapping = {
+            lethargy = "192.168.1.1";
+            "jonsnow.local" = "192.168.1.1";
+            bistannix = "192.168.1.42";
+            "home.nicdumz.fr" = "192.168.1.1";
+          };
         };
-        denyLists = {
-          suspicious = [
-            "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt"
-            "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
-            "https://v.firebog.net/hosts/static/w3kbl.txt"
+        caching.prefetching = true;
+        queryLog = {
+          type = "csv";
+          logRetentionDays = 7;
+          target = "/var/log/blocky";
+        };
+        ports.http = 4000;
+        prometheus.enable = true;
+        blocking = {
+          # NOTE: blocking was disabled for a while.
+          clientGroupsBlock.default = [
+            "ads"
+            "suspicious"
+            "tracking"
           ];
-          ads = [
-            "https://adaway.org/hosts.txt"
-            "https://v.firebog.net/hosts/AdguardDNS.txt"
-            "https://v.firebog.net/hosts/Admiral.txt"
-            "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
-            "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
-            "https://v.firebog.net/hosts/Easylist.txt"
-          ];
-          tracking = [
-            "https://v.firebog.net/hosts/Easyprivacy.txt"
-            "https://v.firebog.net/hosts/Prigent-Ads.txt"
-            "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
-          ];
-          malware = [
-            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
-            "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt"
-            "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
-            "https://v.firebog.net/hosts/Prigent-Crypto.txt"
-          ];
+          # https://firebog.net/ was useful here
+          allowlists = {
+            ads = [
+              # inline definition of hosts to allow
+              ''
+                bit.ly
+                bnc.lt
+                cdn.optimizely.com
+                ow.ly
+                s.shopify.com
+                t.ly
+                tinyurl.com
+                www.bit.ly
+                # https://github.com/StevenBlack/hosts/issues/1206 sigh both insta and
+                # tracking
+                graph.instagram.com
+                # this is what M$ uses to phone home / check connectivity
+                dns.msftncsi.com''
+            ];
+          };
+          denylists = {
+            suspicious = [
+              "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt"
+              "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
+              "https://v.firebog.net/hosts/static/w3kbl.txt"
+            ];
+            ads = [
+              "https://adaway.org/hosts.txt"
+              "https://v.firebog.net/hosts/AdguardDNS.txt"
+              "https://v.firebog.net/hosts/Admiral.txt"
+              "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
+              "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+              "https://v.firebog.net/hosts/Easylist.txt"
+            ];
+            tracking = [
+              "https://v.firebog.net/hosts/Easyprivacy.txt"
+              "https://v.firebog.net/hosts/Prigent-Ads.txt"
+              "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
+            ];
+            malware = [
+              "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
+              "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt"
+              "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
+              "https://v.firebog.net/hosts/Prigent-Crypto.txt"
+            ];
+          };
         };
       };
     };
