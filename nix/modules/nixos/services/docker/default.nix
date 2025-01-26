7c9eaff5b563731a5f52303da52ca96ac3ffb3f5
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-26 20:56:50 +0100

    Move traefik to its own folder.
    Will generate configs later.

--- nix/modules/nixos/services/docker/default.nix
+++ nix/modules/nixos/services/docker/default.nix
@@ -10,12 +10,50 @@ in
 {
   imports = [ ./compose2nix.nix ];
 
-  options.${namespace}.docker = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable Docker on host";
+  options.${namespace} = {
+    docker = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable Docker on host";
+      };
     };
+    containers =
+      let
+        uid = builtins.toString config.users.users.ndumazet.uid;
+        gid = builtins.toString config.users.groups.users.gid;
+      in
+      {
+        defaultEnvironment = lib.mkOption {
+          type = lib.types.attrsOf lib.types.str;
+          description = "Shared enviroment variables for all containers.";
+          default = {
+            PGID = gid;
+            GID = gid;
+            PUID = uid;
+            UID = uid;
+            USERMAP_GID = gid;
+            USERMAP_UID = uid;
+            TZ = config.time.timeZone;
+          };
+        };
+        user = lib.mkOption {
+          type = lib.types.str;
+          description = "User option for all containers. Usually `uid:gid`";
+          default = "${uid}:${gid}";
+        };
+        dataroot = {
+          fast = lib.mkOption {
+            type = lib.types.str;
+            # TODO: Will need to move back to fast
+            default = "/media/bigslowdata/dockerstate";
+          };
+          slow = lib.mkOption {
+            type = lib.types.str;
+            default = "/media/bigslowdata";
+          };
+        };
+      };
   };
   config = lib.mkIf cfg.enable {
     virtualisation.docker = {
