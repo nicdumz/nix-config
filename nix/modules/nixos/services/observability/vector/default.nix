cd2b09088d2f98ed49c2e32c4b903467f9cf69f5
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-29 17:18:40 +0100

    improve structure of logs in Loki

--- nix/modules/nixos/services/observability/vector/default.nix
+++ nix/modules/nixos/services/observability/vector/default.nix
@@ -28,7 +28,7 @@ in
       ];
 
       systemd.services.vector.after = [ "loki.service" ];
-      systemd.services.vector.wants = [ "loki.service" ];
+      systemd.services.vector.requires = [ "loki.service" ];
 
       services.vector = {
         enable = true;
@@ -45,18 +45,97 @@ in
             };
             # TODO: Docker?
           };
+          transforms = {
+            processed_journald = {
+              type = "remap";
+              inputs = [ "systemlog" ];
+              source = ''
+                # delete fields I don't find useful
+                del(.CONTAINER_ID_FULL)
+                del(.CONTAINER_ID)
+                del(.CONTAINER_LOG_EPOCH)
+                del(.CONTAINER_TAG)
+                del(.SYSLOG_FACILITY)
+                del(.SYSLOG_PID)
+                del(.SYSLOG_TIMESTAMP)
+                del(._AUDIT_LOGINUID)
+                del(._AUDIT_SESSION)
+                del(._BOOT_ID)
+                del(._CAP_EFFECTIVE)
+                del(._CMDLINE)
+                del(._COMM)
+                del(._EXE)
+                del(._GID)
+                del(._MACHINE_ID)
+                del(._PID)
+                del(._SELINUX_CONTEXT)
+                del(._SOURCE_MONOTONIC_TIMESTAMP)
+                del(._SOURCE_REALTIME_TIMESTAMP)
+                del(._STREAM_ID)
+                del(._SYSTEMD_CGROUP)
+                del(._SYSTEMD_INVOCATION_ID)
+                del(._SYSTEMD_OWNER_UID)
+                del(._SYSTEMD_SESSION)
+                del(._SYSTEMD_SLICE)
+                del(._SYSTEMD_USER_SLICE)
+                del(._TRANSPORT)
+                del(._UID)
+                del(.__MONOTONIC_TIMESTAMP)
+                del(.__SEQNUM)
+                del(.__SEQNUM_ID)
+                del(.source_type)
+                del(.timestamp)
+
+                if !exists(._SYSTEMD_UNIT) {
+                  ._SYSTEMD_UNIT = "unknown"
+                }
+                .body = del(.)
+                .service_name = del(.body.SYSLOG_IDENTIFIER)
+                .systemd_scope = del(.body._RUNTIME_SCOPE)
+                .systemd_unit = del(.body._SYSTEMD_UNIT)
+                .timestamp_nanos = del(.body.__REALTIME_TIMESTAMP)
+                .container = del(.body.CONTAINER_NAME)
+
+                # parse priority
+                .severity_text = if includes(["0", "1", "2", "3"], .body.PRIORITY) {
+                  "ERROR"
+                } else if .body.PRIORITY == "4" {
+                  "WARN"
+                } else if .body.PRIORITY == "7" {
+                  "DEBUG"
+                } else if includes(["6"], .body.PRIORITY) {
+                  "INFO"
+                } else {
+                  "NOTICE"
+                }
+                del(.body.PRIORITY)
+
+                # deal with firewall messages
+                if .service_name == "kernel" {
+                  p, err = parse_regex(.body.message, r'refused connection: (?P<packet>.*)')
+                  if err == null {
+                    .packet = parse_key_value!(p.packet)
+                    .body.message = "refused connection"
+                    .service_name = "firewall"
+                  }
+                }
+              '';
+            };
+          };
           sinks = {
-            # console_out = {
-            #   type = "console";
-            #   inputs = ["systemlog"];
-            #   encoding.codec = "text";
-            # };
             loki = {
               type = "loki";
-              inputs = [ "systemlog" ];
+              inputs = [ "processed_journald" ];
               endpoint = "http://${loki.bindAddress}:${builtins.toString loki.port}";
               encoding.codec = "json";
-              labels.logsource = "systemlog";
+              labels = {
+                service_name = "{{service_name}}";
+                unit = "{{systemd_unit}}";
+                severity = "{{severity_text}}";
+                firewall_in = "{{packet.IN}}";
+                firewall_out = "{{packet.OUT}}";
+                host = "{{body.host}}";
+              };
             };
           };
         };
