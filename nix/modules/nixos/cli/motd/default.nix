13cde4e7e9ea95a38d5ad595c27d60a4e36680ed
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-01-31 10:56:44 +0100

    motd tweaks: now prettier, and with disk usage

--- nix/modules/nixos/cli/motd/default.nix
+++ nix/modules/nixos/cli/motd/default.nix
@@ -25,25 +25,31 @@ let
     upDays=$((uptime/60/60/24))
     upHours=$((uptime/60/60%24))
     upMins=$((uptime/60%60))
-    upSecs=$((uptime%60))
 
     figlet "$(hostname)" | lolcat -f
     printf "\n"
     ${lib.strings.concatStrings (
       lib.lists.forEach cfg.networkInterfaces (
         x:
-        "printf \"$BOLD  * %-20s$ENDCOLOR %s\\n\" \"IPv4 ${x}\" \"$(ip -4 addr show ${x} | grep -oP '(?<=inet\\s)\\d+(\\.\\d+){3}')\"\n"
+        "printf \"$BOLD  * %-30s$ENDCOLOR %s\\n\" \"IPv4 ${x}\" \"$(ip -4 addr show ${x} | grep -oP '(?<=inet\\s)\\d+(\\.\\d+){3}')\"\n"
       )
     )}
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Release" "$PRETTY_NAME"
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Kernel" "$(uname -rs)"
-    [ -f /var/run/reboot-required ] && printf "$RED  * %-20s$ENDCOLOR %s\n" "A reboot is required"
+    printf "$BOLD  * %-30s$ENDCOLOR %s\n" "Release" "$PRETTY_NAME"
+    printf "$BOLD  * %-30s$ENDCOLOR %s\n" "Kernel" "$(uname -rs)"
+    [ -f /var/run/reboot-required ] && printf "$RED  * %-30s$ENDCOLOR %s\n" "A reboot is required"
     printf "\n"
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "CPU usage" "$LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)"
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Memory" "$MEMORY"
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "System uptime" "$upDays days $upHours hours $upMins minutes $upSecs seconds"
+    printf "$BOLD  * %-30s$ENDCOLOR %s\n" "CPU usage" "$LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)"
+    printf "$BOLD  * %-30s$ENDCOLOR %s\n" "Memory" "$MEMORY"
+    printf "$BOLD  * Disk usage$ENDCOLOR %s\n"
+    ${lib.strings.concatStrings (
+      lib.lists.forEach cfg.fileSystems (
+        x:
+        "printf \"$BOLD    \\055 %-28s$ENDCOLOR %s\\n\" \"${x}\" \"$(df -h ${x} | tail -n1 | awk '{print $5;}')\"\n"
+      )
+    )}
+    printf "$BOLD  * %-30s$ENDCOLOR %s\n" "System uptime" "$upDays days $upHours hours $upMins minutes"
     printf "\n"
-    printf "$BOLDService status$ENDCOLOR\n"
+    printf "$BOLD Service status$ENDCOLOR\n"
 
     while IFS= read -r line; do
       if [[ $line =~ ".scope" ]]; then
@@ -51,11 +57,11 @@ let
       fi
       service_name=$(echo $line | awk '{print $1;}')
       if echo "$line" | grep -q 'failed'; then
-        printf "$RED• $ENDCOLOR%-50s $RED[failed]$ENDCOLOR\n" "$service_name"
+        printf " $RED• $ENDCOLOR%-50s $RED[failed]$ENDCOLOR\n" "$service_name"
       elif echo "$line" | grep -q 'running'; then
-        printf "$GREEN• $ENDCOLOR%-50s $GREEN[active]$ENDCOLOR\n" "$service_name"
+        printf " $GREEN• $ENDCOLOR%-50s $GREEN[active]$ENDCOLOR\n" "$service_name"
       else
-        printf "• $ENDCOLOR%-50s [unknown]\n" "$service_name"
+        printf " • $ENDCOLOR%-50s [unknown]\n" "$service_name"
       fi
     done <<< "$service_status"
   '';
@@ -69,6 +75,14 @@ in
       description = "Network interfaces to display";
       type = lib.types.listOf lib.types.str;
       default = [ ];
+    };
+    fileSystems = lib.mkOption {
+      description = "Filesystems to report on.";
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "/media/bigslowdata"
+        "/nix"
+      ];
     };
     systemdServices = lib.mkOption {
       description = "Systemd service units to watch for.";
