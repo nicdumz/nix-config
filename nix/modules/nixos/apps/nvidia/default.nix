0971dad1627672da5bc3d312e283b81351a931dd
Nicolas Dumazet <nicdumz.commits@gmail.com>
2025-12-01 22:32:50 +0100

    re-enable nvidia as nouveau drivers are toasted on wayland

--- nix/modules/nixos/apps/nvidia/default.nix
+++ nix/modules/nixos/apps/nvidia/default.nix
@@ -17,31 +17,34 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    # 25.05: IMO it's strange to have this configured here when the OS runs wayland.
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
-      # Modesetting is required, for wayland etc.
-      modesetting.enable = true;
       # NOTE: if off, then screen-tearing appears when waking up from sleep. If it's on, random
       # crashes happen when waking up. Nothing actually works :))
-      powerManagement.enable = true; # Try that to avoid tearing?
+      # powerManagement.enable = true; # Try that to avoid tearing?
       open = true; # required for RTX
+
+      # The following are now defaults AFAICT
+      # # Modesetting is required, for wayland etc.
+      # # modesetting.enable = true;
     };
 
-    # TODO: package in a separate module if I keep that.
-    services.ollama = {
-      enable = true;
-      acceleration = "cuda";
-      loadModels = [ "qwen2.5-coder:1.5b" ];
-    };
+    # # TODO: package in a separate module if I keep that.
+    # services.ollama = {
+    #   enable = true;
+    #   acceleration = "cuda";
+    #   loadModels = [ "qwen2.5-coder:1.5b" ];
+    # };
 
     nixpkgs.config.allowUnfreePredicate =
       pkg:
       builtins.elem (lib.getName pkg) [
-        # Cuda (for ollama) is unfree.
-        "cuda_cccl"
-        "cuda_cudart"
-        "cuda_nvcc"
-        "libcublas"
+        #   # Cuda (for ollama) is unfree.
+        #   "cuda_cccl"
+        #   "cuda_cudart"
+        #   "cuda_nvcc"
+        #   "libcublas"
         # nvidia drivers
         "nvidia-x11"
         "nvidia-settings"
