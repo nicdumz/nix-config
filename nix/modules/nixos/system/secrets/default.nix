6927b9ed674176f1fe77a17fa3145911d3982e00
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-01-11 13:02:49 +0100

    Add a GH token to nix commands.
    So things like `nix flake update` are not rate limited.

--- nix/modules/nixos/system/secrets/default.nix
+++ nix/modules/nixos/system/secrets/default.nix
@@ -1,4 +1,9 @@
-{ inputs, pkgs, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
 {
 
   imports = [ inputs.sops-nix.nixosModules.sops ];
@@ -10,4 +15,13 @@
   environment.systemPackages = with pkgs; [
     libfido2 # provides fido2-token utility
   ];
+
+  # TODO: this could be in home-manager somewhere, but requires setting sops + HM
+  sops.secrets.ndumazet_github_token.restartUnits = [ "nix-daemon.service" ];
+  sops.templates.ndumazet_nix_extra_config = {
+    content = ''
+      access-tokens = github.com=${config.sops.placeholder.ndumazet_github_token}
+    '';
+    owner = "ndumazet";
+  };
 }
