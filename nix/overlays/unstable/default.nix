1da9c60fb3b6d5a7881f9ca012d18533667b4f75
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-01-11 17:20:42 +0100

    jackett pkg seems broken/builds fail.

--- nix/overlays/unstable/default.nix
+++ nix/overlays/unstable/default.nix
@@ -6,4 +6,6 @@
 _final: _prev: {
   # Always pick up the latest VSCodium. Helps with extension compatibility.
   inherit (channels.nixpkgs-unstable) vscodium;
+  # Downgrade after https://github.com/NixOS/nixpkgs/issues/478145
+  inherit (channels.nixpkgs-unstable) jackett;
 }
