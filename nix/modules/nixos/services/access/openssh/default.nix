d57eba3f460e26fdcb6872fd936955539769a3c2
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-01-17 20:15:05 +0100

    sshd persistence

--- nix/modules/nixos/services/access/openssh/default.nix
+++ nix/modules/nixos/services/access/openssh/default.nix
@@ -7,4 +7,5 @@
   # Default opens for all interfaces and it's not smart for the router.
   # TODO: sort of brittle condition, think about multiple interfaces etc.
   services.openssh.openFirewall = config.nicdumz.firewall.interface == "";
+  systemd.services.sshd.unitConfig.RequiresMountsFor = [ "/etc/ssh" ];
 }
