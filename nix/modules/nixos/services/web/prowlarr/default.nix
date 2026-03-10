ab1d5dbcfd97666d1d37022f2bdebe2cd4517af8
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-03-10 21:42:37 +0100

    prowlarr

---
+++ nix/modules/nixos/services/web/prowlarr/default.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  cfg = config.${namespace}.prowlarr;
+in
+{
+  options.${namespace}.prowlarr = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Prowlarr: indexing.";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.prowlarr.enable = true;
+
+    users.groups.media = { };
+    users.users.prowlarr = {
+      description = "Services needs a user but doesn't explicitly create it.";
+      group = "media";
+      isSystemUser = true;
+    };
+
+    # TODO: upstream
+    systemd.services.prowlarr = {
+      unitConfig.RequiresMountsFor = [ config.services.prowlarr.dataDir ];
+      serviceConfig.DynamicUser = lib.mkForce false;
+    };
+    ${namespace} = {
+      motd.systemdServices = [ "prowlarr" ];
+      traefik.webservices.prowlarr.port = config.services.prowlarr.settings.server.port;
+      persistence.directories = [
+        {
+          directory = config.services.prowlarr.dataDir;
+          user = config.users.users.prowlarr.name;
+          inherit (config.users.users.prowlarr) group;
+        }
+      ];
+    };
+  };
+}
--- nix/overlays/unstable/default.nix
+++ nix/overlays/unstable/default.nix
@@ -6,8 +6,6 @@
 _final: _prev: {
   # Always pick up the latest VSCodium. Helps with extension compatibility.
   inherit (channels.nixpkgs-unstable) vscodium;
-  # Downgrade after https://github.com/NixOS/nixpkgs/issues/478145
-  inherit (channels.nixpkgs-unstable) jackett;
   inherit (channels.nixpkgs-unstable) home-assistant;
   # The version in stable is insecure
   inherit (channels.nixpkgs-unstable) fosrl-pangolin;
