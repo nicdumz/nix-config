ab876b6fdfe4cc50a0e9693adff8cc76e677b901
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-03-15 23:25:09 +0100

    wifi for lethargyfamily

--- nix/systems/x86_64-linux/lethargyfamily/default.nix
+++ nix/systems/x86_64-linux/lethargyfamily/default.nix
@@ -15,6 +15,10 @@
     device = "/dev/disk/by-id/SAMSUNG_MZAL41T0HBLB-00BLL_S75WNE0W807108";
   };
 
+  networking.networkmanager.enable = true;
+  # Would be for homemanager
+  # services.network-manager-applet.enable = true;
+
   ${namespace} = {
     graphical = true;
     persistence.enable = true;
