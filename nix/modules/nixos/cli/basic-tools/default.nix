e45bd56b891de3b2f45e5043dd5845aeec659a15
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-04-02 03:31:34 +0200

    feat: more basic tools
    Moved to h-m instead of system config so h-m has them too.

--- nix/modules/nixos/cli/basic-tools/default.nix
+++ nix/modules/nixos/cli/basic-tools/default.nix
@@ -1,16 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   security.sudo.extraConfig = "Defaults insults,timestamp_timeout=30";
-
-  environment.systemPackages = with pkgs; [
-    colordiff
-    git
-    git-crypt
-    htop
-    killall
-    kitty.terminfo
-    tcpdump
-    unzip
-    wget
-  ];
 }
