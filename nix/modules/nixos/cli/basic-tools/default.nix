cc1e2aac9daf315d2d26c8aac8c904d42120c858
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-05-25 15:07:59 +0200

    fix: some packages should be global

--- nix/modules/nixos/cli/basic-tools/default.nix
+++ nix/modules/nixos/cli/basic-tools/default.nix
@@ -1,3 +1,18 @@
-_: {
+{ pkgs, ... }:
+{
   security.sudo.extraConfig = "Defaults insults,timestamp_timeout=30";
+
+  # Stuff that root user and others may need
+  environment.systemPackages = with pkgs; [
+    colordiff
+    dig
+    ethtool
+    git
+    htop
+    killall
+    kitty.terminfo
+    tcpdump
+    unzip
+    wget
+  ];
 }
