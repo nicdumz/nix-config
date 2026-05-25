62af94a8eaad46fcee0cd805935fdacd12ae31ea
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-05-25 15:07:59 +0200

    fix: some packages should be global

--- nix/modules/home/cli/basic-tools/default.nix
+++ nix/modules/home/cli/basic-tools/default.nix
@@ -1,20 +1,10 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    colordiff
-    dig
-    ethtool
-    git
     git-crypt
     glow
-    htop
-    killall
-    kitty.terminfo
     nixd
     nixfmt-rfc-style
     nixpkgs-review
-    tcpdump
-    unzip
-    wget
   ];
 }
