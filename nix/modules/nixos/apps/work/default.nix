9663b22827eb49ac83cb25091841dd4de6e0ab5e
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-06-08 14:29:24 +0200

    feat: s/slack/work apps/

---
+++ nix/modules/nixos/apps/work/default.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  namespace,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.${namespace};
+in
+{
+  options.${namespace} = {
+    work = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable work apps.";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.work.enable {
+    services.tailscale.enable = true;
+    environment.systemPackages =
+      with pkgs;
+      [
+        bazelisk
+        buildifier
+        uv
+        # aspect-cli
+        tailscale
+      ]
+      ++ (lib.optionals cfg.device.isGraphical [ slack ]);
+  };
+}
