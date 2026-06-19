6b00394e9d91782d7c8e309a05212438339c2297
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-06-19 11:14:36 +0200

    fix: disable USB autosuspend some usb devices

--- nix/modules/nixos/services/audio/default.nix
+++ nix/modules/nixos/services/audio/default.nix
@@ -6,5 +6,13 @@
       enable = true;
       pulse.enable = true;
     };
+
+    # Prevent USB hub and webcam from autosuspending mid-call (causes camera drops in Google Meet).
+    # The Realtek hub (0bda:0420) power-cycles all downstream devices when it suspends,
+    # triggering uvcvideo URB errors and /dev/video0 disappearing briefly.
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="0420", ATTR{power/control}="on"
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="0893", ATTR{power/control}="on"
+    '';
   };
 }
