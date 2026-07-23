f630d6007cba696e084d78223dbff754793a3cf9
Nicolas Dumazet <nicdumz.commits@gmail.com>
2026-07-23 22:10:04 +0200

    docs: Garnix -> Nix CI

--- templates/corp/home.nix
+++ templates/corp/home.nix
@@ -34,7 +34,6 @@ in
   nix.settings = {
     substituters = [
       "https://cache.nixos.org"
-      "https://cache.garnix.io"
       "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
       "https://noctalia.cachix.org"
@@ -44,7 +43,6 @@ in
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
